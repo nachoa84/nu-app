cfe5cb0530cf6992a8063b9488bba0cd7af9d306
Nacho Alderete <118692291+nachoa84@users.noreply.github.com>
2026-08-31 09:40:39 -0300

    Refresh routine card cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="nuapp-v154-five-line-routine-cards";
+const CACHE="nuapp-v155-routine-cards-clean";
 
 const CORE=[
   "./",
@@ -10,7 +10,7 @@ const CORE=[
   "./navigation.css?v=81-home-navigation-legibility",
   "./app-navigation-floating-v1.css?v=20260830-floating-nav-v1",
   "./typography-poppins-v1.css?v=20260830-poppins-v1",
-  "./daily.css?v=99-five-line-cards",
+  "./daily.css?v=100-routine-cards-clean",
   "./progress.css?v=83-routine-progress-legibility",
   "./media-preview.css?v=85a-media-legibility",
   "./shared.css?v=87-shared-ui-legibility",
